41d9c46b238198d9fec57d65b8d59775c25120f8
chart099 <122659980+chart099@users.noreply.github.com>
2023-12-01 18:01:28 -0600

    updated README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React-Portfolio
 
 This is an online portfolio to showcase both work history and current React skills. It has multiple pages that can be clicked through to see different aspects of the portfolio. 
+
+Live link: https://beamish-mousse-5946e7.netlify.app/
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -4,17 +4,17 @@ export default function PortfolioPage() {
             <h2>Portfolio</h2>
             <div id="project-container">
 
-                    <img src="/src/assets/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
 
-                    <img src="/src/assets/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
 
-                    <img src="/src/assets/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
 
-                    <img src="/src/assets/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
                
-                    <img src="/src/assets/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
            
-                    <img src="/src/assets/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
             
             </div>
         </div>
