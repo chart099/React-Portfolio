2247fcda21d4950ddb6af847ce48a33a2ecc772d
chart099 <122659980+chart099@users.noreply.github.com>
2023-12-01 23:00:09 -0600

    fixed portfolio page labels

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -3,19 +3,36 @@ export default function PortfolioPage() {
         <div className="d-flex flex-column" style={{height: "100vh"}}>
             <h2>Portfolio</h2>
             <div id="project-container">
-
-                    <a href="https://github.com/chart099/Social-Media-API" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
-
+                    <div>
+                    <h6>Social Media API</h6>
+                    <a href="https://github.com/chart099/Social-Media-API" target="_blank"> <img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
+                    
+                    </div>
+                    <div>
+                    <h6>SQL Employee Tracker</h6>
                    <a href="https://github.com/chart099/SQL-Employee-Tracker" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
-
+                   
+                   </div>
+                   <div>
+                   <h6>Note Taker</h6>
                    <a href="https://github.com/chart099/Note-Taker" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
-
+                    <h6>Note Taker</h6>
+                    </div>
+                    <div>
+                    <h6>SVG Generator</h6>
                    <a href="https://github.com/chart099/SVG-Generator" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
-               
+                    
+                    </div>
+                    <div>
+                    <h6>E-Commerce-Back End</h6>
                    <a href="https://github.com/chart099/E-Commerce-Back-End" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
-           
+                    
+                    </div>
+                    <div>
+                    <h6>JS Password Generator</h6>
                    <a href="https://github.com/chart099/Password-Generator" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
-            
+                    
+                    </div>
             </div>
         </div>
     );
