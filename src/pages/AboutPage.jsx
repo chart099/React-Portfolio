7b1d56af1dbc0d01451d73bef50db24d37dc10a1
chart099 <122659980+chart099@users.noreply.github.com>
2023-12-01 23:06:30 -0600

    trying to fix img links

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,7 @@ export default function AboutPage() {
         <div >
             <h2>About Me</h2>
             <div className="d-flex justify-content-center">
-                <img src="public\pothos stock image.jpg" alt="photo of a plant" class="img-fluid w-50"/>
+                <img src="public/pothos stock image.jpg" alt="photo of a plant" class="img-fluid w-50"/>
             </div>
             
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur provident debitis reprehenderit, beatae temporibus dicta ratione doloremque magnam nostrum illo voluptates quae voluptatem, vitae minus rem labore impedit corrupti voluptas!</p>
