71f9a7cb8c4769d06671c7b5203d18ee1aa626e7
chart099 <122659980+chart099@users.noreply.github.com>
2023-12-01 22:38:44 -0600

    updated links

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -4,17 +4,17 @@ export default function PortfolioPage() {
             <h2>Portfolio</h2>
             <div id="project-container">
 
-                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                    <a href="https://github.com/chart099/Social-Media-API" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
 
-                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                   <a href="https://github.com/chart099/SQL-Employee-Tracker" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
 
-                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                   <a href="https://github.com/chart099/Note-Taker" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
 
-                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                   <a href="https://github.com/chart099/SVG-Generator" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
                
-                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                   <a href="https://github.com/chart099/E-Commerce-Back-End" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
            
-                    <img src="/public/gettyimages-535911393-612x612.jpg" alt="abstract color blobs" />
+                   <a href="https://github.com/chart099/Password-Generator" target="_blank"><img src="public\gettyimages-535911393-612x612.jpg" alt="abstract color blobs" /></a>
             
             </div>
         </div>
